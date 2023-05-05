cde90b86539f039ef8bf484d08c8dc0e037b29cb
ambrosashvili1 <nambrosashvili@gmail.com>
2023-05-05 12:55:41 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+function changeStyle(){
+    var element = document.getElementById("link");
+    element.style.border = "2px dotted black";
+}
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
